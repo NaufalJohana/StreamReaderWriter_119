e122bb52254eba031279700e6350446b409199d3
NaufalJohana <naufaljohana@gmail.com>
2023-06-19 11:43:52 +0700

    membuat infile close

--- ioFileWithpath/ioFileWithpath.cpp
+++ ioFileWithpath/ioFileWithpath.cpp
@@ -38,4 +38,15 @@ int main() {
 	if (infile.is_open())
 	{
 		//melakukan perulangan setiap baris
-		
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+}
