ae3cd5d828424db35b43bfb81f701a0295b6fe0f
NaufalJohana <naufaljohana@gmail.com>
2023-06-19 11:47:16 +0700

    membuat try array awal program

--- exceptionStarndar/exceptionStarndar.cpp
+++ exceptionStarndar/exceptionStarndar.cpp
@@ -4,3 +4,12 @@
 #include <array>
 //untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+	cout << "awal program" << endl; // penanda 1:...
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan array integer 3 elemen
+		//cout<<data.at(5)<<endl;
+		//memanggil array elemen ke 5
