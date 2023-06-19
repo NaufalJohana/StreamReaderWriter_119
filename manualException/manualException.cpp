883f99ee85669a5feea9db00aecc1c2b4dc0213b
NaufalJohana <naufaljohana@gmail.com>
2023-06-19 11:45:17 +0700

    membuat catch int a block ini akan di eksekusi

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -7,4 +7,8 @@ int main()
 		cout << "Selamat Belajar di Prodi TI UMY" << endl;
 		//throw 0.5 //melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		//block ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
 	
