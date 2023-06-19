380cab5c9f55224cb7aaccbec90eeffbdc63ac31
NaufalJohana <naufaljohana@gmail.com>
2023-06-19 11:39:27 +0700

    tutup file stelah selesai

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -36,4 +36,11 @@ int main() {
 		//melakukan perulangan setiap baris
 		while (getline(infile, baris))
 		{
-			
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	
+
