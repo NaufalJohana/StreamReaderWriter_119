d09c2dad59666e9d9fadb4559c36d12fd022095d
NaufalJohana <naufaljohana@gmail.com>
2023-06-19 11:43:31 +0700

    membuat if infile is open

--- ioFileWithpath/ioFileWithpath.cpp
+++ ioFileWithpath/ioFileWithpath.cpp
@@ -33,5 +33,9 @@ int main() {
 	//menunjuk ke sebuah file
 	infile.open(NamaFile, ios::in);
 
-	
-}
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		
